6349b5442f0d8aba9b46640edba83c7708a19d88
Marty Trujillo <marty@ilikeemail.com>
2025-07-20 20:52:57 -0400

    Converts from callbacks to async await.

--- api/services/filesystemService.js
+++ api/services/filesystemService.js
@@ -1,15 +1,12 @@
-const fs = require('fs');
 const path = require('path');
-const util = require('util');
-
-const readdir = util.promisify(fs.readdir);
+const fs = require('fs').promises;
 
 const INCOMING_DIR = path.resolve(__dirname, '../../incoming');
 const allowedExtensions = require('../config/mediaTypes.json');
 
 async function getMediaFiles() {
   try {
-    const files = await readdir(INCOMING_DIR);
+    const files = await fs.readdir(INCOMING_DIR);
     return files.filter(file => {
       const ext = path.extname(file).toLowerCase();
       return allowedExtensions.includes(ext);
@@ -19,40 +16,36 @@ async function getMediaFiles() {
   }
 }
 
-function getIncomingMediaMeta(callback) {
-  fs.readdir(INCOMING_DIR, (err, files) => {
-    if (err) return callback(err);
-
-    const mediaFiles = files.filter(file => {
-      const ext = path.extname(file).toLowerCase();
-      return allowedExtensions.includes(ext);
-    });
+async function getIncomingMediaMeta() {
+  try {
+    const mediaFiles = await getMediaFiles();
 
     if (mediaFiles.length === 0) {
-      return callback(null, { count: 0, totalSize: 0, totalSizeMb: 0 });
+      return { count: 0, totalSize: 0, totalSizeMb: 0 };
     }
 
     let totalSize = 0;
-    let processed = 0;
 
-    mediaFiles.forEach(file => {
+    for (const file of mediaFiles) {
       const fullPath = path.join(INCOMING_DIR, file);
-      fs.stat(fullPath, (err, stats) => {
-        processed++;
-        if (!err && stats.isFile()) {
+      try {
+        const stats = await fs.stat(fullPath);
+        if (stats.isFile()) {
           totalSize += stats.size;
         }
-        if (processed === mediaFiles.length) {
-          const totalSizeMb = totalSize / 1024 / 1024;
-          callback(null, {
-            count: mediaFiles.length,
-            totalSize,
-            totalSizeMb
-          });
-        }
-      });
-    });
-  });
+      } catch (err) {
+        console.warn(`Failed to stat file ${fullPath}:`, err);
+      }
+    }
+
+    return {
+      count: mediaFiles.length,
+      totalSize,
+      totalSizeMb: totalSize / 1024 / 1024,
+    };
+  } catch (err) {
+    throw new Error('Unable to read meta data');    
+  }
 }
 
 module.exports = { getMediaFiles, getIncomingMediaMeta };
