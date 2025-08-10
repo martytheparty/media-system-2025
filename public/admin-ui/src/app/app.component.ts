667cd77b29f94d5f0956077da8a960f55fa3da61
Marty Trujillo <marty@ilikeemail.com>
2025-08-10 19:36:20 -0400

    Issue 17 - allows the user to initialize or update the gallery name.

--- public/admin-ui/src/app/app.component.ts
+++ public/admin-ui/src/app/app.component.ts
@@ -1,15 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
-import { RouterOutlet } from '@angular/router';
-import { RouterModule } from '@angular/router';
+import { RouterOutlet, Router, RouterModule } from '@angular/router';
+
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { MatButtonModule } from '@angular/material/button';
-
-
-
-
+import { ApiService } from './services/api.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -22,6 +20,31 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+
+  apiService: ApiService = inject(ApiService);
+  sub: Subscription;
+  router: Router = inject(Router);
+
+
+  constructor() {
+    console.log("Constructor");
+    this.sub = this.apiService.isInitialized().subscribe(
+      (initialized: boolean) => {
+        if (!initialized) {
+          this.router.navigate(['/config']);
+        }
+      }
+    );
+  }
+
+
+  ngOnInit(): void {
+    
+  }
+
+  ngOnDestroy(): void {
+    
+  }
   title = 'admin-ui';
 }
