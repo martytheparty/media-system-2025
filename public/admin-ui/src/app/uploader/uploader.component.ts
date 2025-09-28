e88f750ba7e69ab96c1adba54c5e1e3665fffcee
Marty Trujillo <marty@ilikeemail.com>
2025-09-27 22:04:02 -0400

    Issue 43 - updates the uploader to display the gallery data.

--- public/admin-ui/src/app/uploader/uploader.component.ts
+++ public/admin-ui/src/app/uploader/uploader.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { take } from 'rxjs';
+import { GalleryData } from '../interfaces/gallery-data-response.interface';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-uploader',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './uploader.component.html',
   styleUrl: './uploader.component.scss'
 })
 export class UploaderComponent {
+
+    apiService: ApiService = inject(ApiService);
+
+    galleryData: GalleryData | undefined;
+
+    constructor() {
+      this.apiService.getGalleryData().pipe(take(1)).subscribe(
+        (data: GalleryData) => {
+          this.galleryData = data;
+        } 
+      );
+    }
 
 }
