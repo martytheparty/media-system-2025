e7012aca96e726ddb5a302a5a5124306b536b5cb
Marty Trujillo <marty@ilikeemail.com>
2025-10-19 20:33:04 -0400

    Issue 57 - adds ftp config route and hello world component.

--- public/admin-ui/src/app/app.routes.ts
+++ public/admin-ui/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Routes } from '@angular/router';
 import { IncomingComponent } from './incoming/incoming.component';
 import { UploaderComponent } from './uploader/uploader.component';
 import { ConfigComponent } from './config/config.component';
+import { FtpconfigComponent } from './ftpconfig/ftpconfig.component';
 
 export const routes: Routes = [
   { path: 'incoming', component: IncomingComponent },
   { path: 'uploader', component: UploaderComponent },
   { path: 'config', component: ConfigComponent },
+  { path: 'ftpconfig', component: FtpconfigComponent },
   { path: '', redirectTo: '/incoming', pathMatch: 'full' },
 ];
 
---
+++ public/admin-ui/src/app/ftpconfig/ftpconfig.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ftpconfig',
+  imports: [],
+  templateUrl: './ftpconfig.component.html',
+  styleUrl: './ftpconfig.component.scss'
+})
+export class FtpconfigComponent {
+
+}
