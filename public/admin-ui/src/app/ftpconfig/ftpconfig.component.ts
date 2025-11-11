bf02e374e7943a78fe7b4c3bf4821932dcad8aa0
Marty Trujillo <marty@ilikeemail.com>
2025-11-11 15:33:04 -0500

    Issue 91 - Adds the ability to test the host to the UI.

--- public/admin-ui/src/app/ftpconfig/ftpconfig.component.ts
+++ public/admin-ui/src/app/ftpconfig/ftpconfig.component.ts
@@ -1,12 +1,14 @@
 import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { ApiService } from '../services/api.service';
-import { FtpConfigData } from '../interfaces/gallery-data-response.interface';
+import { FtpConfigData, HostResult } from '../interfaces/gallery-data-response.interface';
 import { take } from 'rxjs';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule,  MatDialog, MatDialogRef  } from '@angular/material/dialog';
+import { MatIcon } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ftpconfig',
@@ -15,7 +17,9 @@ import { MatDialogModule,  MatDialog, MatDialogRef  } from '@angular/material/di
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatIcon,
+    CommonModule
   ],
   templateUrl: './ftpconfig.component.html',
   styleUrl: './ftpconfig.component.scss'
@@ -40,6 +44,8 @@ export class FtpconfigComponent {
   });
 
   found = false;
+
+  hostStatus: 'unknown' | 'ok' | 'fail' = 'unknown';
 
   constructor(private dialog: MatDialog) {
     this.getConfigData();
@@ -96,6 +102,25 @@ export class FtpconfigComponent {
   }
 
   closeDialog() {
+    this.hostStatus = 'unknown';
     this.dialogRef.close();
+  }
+
+  testHost() {
+    const host = this.form.controls['host'].value;
+
+    if (host)
+    {
+      this.apiService.getCheckHost(host).pipe(take(1)).subscribe(
+        (result: HostResult) => {
+          if (result.success) {
+            this.hostStatus = "ok";
+          } else {
+            this.hostStatus = "fail";
+          }
+
+        }
+      );
+    }
   }
 }
